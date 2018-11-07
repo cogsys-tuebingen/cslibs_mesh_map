138b82887ae430c38a68276d715b54c836bc3ecd
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2018-11-07 18:10:10 +0100

    added some stuff for compatibility with muse, to be tested if edge particles directly work...

--- include/cslibs_mesh_map/edge_particle.h
+++ include/cslibs_mesh_map/edge_particle.h
@@ -5,41 +5,45 @@
 #include <tf/tf.h>
 namespace cslibs_mesh_map {
 
-struct EdgeParticle
+struct EIGEN_ALIGN16 EdgeParticle
 {
-  EdgeParticle();
-  void setVertices(MeshMap& map,
-                   MeshMap::VertexIterator active_vertex,
-                   MeshMap::VertexOutHalfedgeIterator goal_vertex);
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    using allocator_t = Eigen::aligned_allocator<EdgeParticle>;
+    using Ptr         = std::shared_ptr<EdgeParticle>;
 
-  void setVertices(MeshMap& map,
-                   std::size_t active_vertex,
-                   MeshMap::VertexOutHalfedgeIterator goal_vertex);
+    EdgeParticle();
+    void setVertices(MeshMap& map,
+                     MeshMap::VertexIterator active_vertex,
+                     MeshMap::VertexOutHalfedgeIterator goal_vertex);
 
-  void setVertices(MeshMap& map,
-                   std::size_t active_vertex);
+    void setVertices(MeshMap& map,
+                     std::size_t active_vertex,
+                     MeshMap::VertexOutHalfedgeIterator goal_vertex);
 
-  void setVertices(MeshMap& map,
-                   MeshMap::VertexHandle active_vertex);
+    void setVertices(MeshMap& map,
+                     std::size_t active_vertex);
 
-  cslibs_math_3d::Vector3d getPosition(const MeshMap& map) const;
-  cslibs_math_3d::Vector3d getActiveVertex(const MeshMap& map) const;
-  cslibs_math_3d::Vector3d getGoalVertex(const MeshMap& map) const;
-  void updateEdgeLength(const MeshMap& map);
+    void setVertices(MeshMap& map,
+                     MeshMap::VertexHandle active_vertex);
 
-  double getDistanceToGoal() const;
+    cslibs_math_3d::Vector3d getPosition(const MeshMap& map) const;
+    cslibs_math_3d::Vector3d getActiveVertex(const MeshMap& map) const;
+    cslibs_math_3d::Vector3d getGoalVertex(const MeshMap& map) const;
+    void updateEdgeLength(const MeshMap& map);
 
-  double getDistanceFromStart() const;
+    double getDistanceToGoal() const;
 
-  cslibs_math_3d::Vector3d  getNormal(const MeshMap &map) const;
+    double getDistanceFromStart() const;
 
-  MeshMap::VertexHandle active_vertex;
-  MeshMap::VertexHandle goal_vertex;
-  double s;
-  double e;
-  double weight;
-  std::size_t map_id;
+    cslibs_math_3d::Vector3d  getNormal(const MeshMap &map) const;
+
+    MeshMap::VertexHandle active_vertex;
+    MeshMap::VertexHandle goal_vertex;
+    double s;
+    double e;
+    double weight;
+    std::size_t map_id;
 };
-
 } // namespace cslibs_mesh_map
+
 #endif // EDGE_PARTICLE_H
